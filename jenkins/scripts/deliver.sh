7adc8964dc56bc9a5c3a35911840910325137a19
Nouman72884 <noumansatti7@gmail.com>
2020-12-03 00:37:53 +0500

    hello world java

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,8 +3,12 @@ echo "executing script"
 sudo apt-get update -y
 sudo apt-get install default-jre -y
 sudo apt-get install apache2 -y
+echo "entering html"
 chmod 777 /var/www/html
 sudo cp /tmp/tmp/app/* /var/www/html
+ls
 cd  /var/www/html
+ls
 sudo rm index.html
+ls
 nohup java -jar spring-boot-hello-world-1.0.0-SNAPSHOT.jar > index.html 2>&1 &
