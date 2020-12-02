7011c4c8f8d4749fc98af582f7b9d8f788695682
Nouman72884 <noumansatti7@gmail.com>
2020-12-03 03:39:47 +0500

    hello world java

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,7 +5,7 @@ sudo apt-get install default-jre -y
 sudo apt-get install apache2 -y
 echo "entering html"
 chmod 777 /var/www/html
-sudo cp /tmp/tmp/app/* /var/www/html
+sudo cp my-app-1.0-SNAPSHOT.jar /var/www/html
 ls
 echo "deploying app"
 cd  /var/www/html
